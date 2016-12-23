259b5f52f94178f7ba7feba8f32e0270b45e6eb9
Stephanie Baltus <stephaniedenazareth@gmail.com>
2016-12-23 12:20:01 +0100

    File saving script

---
+++ copy_keepass.sh
@@ -0,0 +1,41 @@
+DST1=/Volumes/myfiles/
+DST2=/Volumes/FILESHARING/
+SRV1=bikette01.ipsyn.net
+SSH_USERNAME=bikette
+SAVING_FILENAME=$1
+
+echo $SAVING_FILENAME
+if [ -f $DST1 ]
+then
+  cp -f $SAVING_FILENAME $DST1
+  result1=OK
+else
+  result1=KO
+fi
+if [ -f $DST2 ]
+then
+    cp -f $SAVING_FILENAME $DST2
+    result2=OK
+  else
+    result2=KO
+fi
+
+ssh -q $SSH_USERNAME@$SRV1 exit
+if [ $? -eq 0 ]
+then
+  scp $SAVING_FILENAME $SSH_USERNAME@$SRV1:backup/.
+  result3=OK
+else
+  result3=KO
+fi
+
+if [ $result1 = $result2 ] && [ $result1 = $result3 ] && [ $result1 = KO ]
+then
+  subject="File $SAVING_FILENAME not saved"
+  body="Could not save $SAVING_FILENAME, no saving location available"
+  echo  $body | mail -s $subject steph.baltus@gmail.com
+else
+  subject="File $SAVING_FILENAME saved"
+  body="Save $SAVING_FILENAME, at least 1 saving location available"
+  echo  $body | mail -s "$subject" steph.baltus@gmail.com
+fi
